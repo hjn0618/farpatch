3baf16a87e3f48ef753ac36549b18512ca9a2ec2
Sean Cross <sean@xobs.io>
2025-12-11 11:26:29 +0800

    blackmagic: remove upstream stdio_newlib.h
    Remove this file as it forces the use of integer versions of stdlib
    functions that don't exist in esp-idf.
    
    Replace the file locally with a version that does not do any replacing.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

---
+++ components/blackmagic/stdio_newlib.h
@@ -0,0 +1,7 @@
+/* This file is empty. It is included within the Blackmagic
+ * project and overwrites various printf functions with their
+ * integer equivalents.
+ * 
+ * These don't exist on ESP-IDF. Use the full-fat floating
+ * point versions on this target.
+ */
